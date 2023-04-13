9f2fe70b70e425d47b96231eacb86d781cfd6121
DVorortnoy <dimchik505@gmail.com>
2023-04-13 11:26:49 +0300

    add loading

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,14 +3,17 @@ import ContactList from '../ContactList/ContactList';
 import Section from '../Section/Section';
 import Filter from '../Filter/Filter';
 import { AppStyled } from './App.styled';
+import { useSelector } from 'react-redux';
+
 
 
 export function App() {
-
+  const isLoading = useSelector(state =>state.contacts.isLoading)
+console.log(isLoading)
   return (
     <AppStyled>
       <Section title="Phonebook">
-        <Form />
+        {!isLoading? <Form />:<h1>...Downloading</h1>}
       </Section>
       <Section title="Contacts">
         <Filter/>
